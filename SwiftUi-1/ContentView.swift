2e06da4d5708a253341935348d7eeb623a83fcbe
Chaman Gurjar <chamangujjar162@gmail.com>
2019-10-06 10:40:00 +0530

    Project Created

---
+++ SwiftUi-1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUi-1
+//
+//  Created by Chaman Gurjar on 06/10/19.
+//  Copyright © 2019 ChamanGurjar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
